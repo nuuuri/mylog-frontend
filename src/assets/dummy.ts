a2ed58d1e8ef52900df914ee122ca51f2bd82a63
nuuuri <sbfl125@gmail.com>
2022-11-23 12:46:40 +0900

    [feat] DummyData 추가

---
+++ src/assets/dummy.ts
@@ -0,0 +1,83 @@
+export const user = {
+  uuid: 1,
+  id: "sbfl124",
+  password: "1234",
+  name: "nuuuri",
+  email: "sbfl125@gmail.com",
+  profileImage: null,
+  created: "2022-11-23 12:10",
+  modified: "2022-11-23 12:15",
+};
+
+export const posts = [
+  {
+    id: 1,
+    writer: 1, // user_uuid
+    category: "test",
+    title: "첫번째 게시물입니다.",
+    content: "test test test",
+    created: "2022-11-23 12:17",
+    modified: "2022-11-23 12:38",
+    tags: ["test", "테스트"],
+  },
+  {
+    id: 2,
+    writer: 1, // user_uuid
+    category: "test",
+    title: "첫번째 게시물입니다.",
+    content: "test test test",
+    created: "2022-11-23 12:17",
+    modified: "2022-11-23 12:38",
+    tags: ["test", "테스트"],
+  },
+  {
+    id: 3,
+    writer: 1, // user_uuid
+    category: "test",
+    title: "첫번째 게시물입니다.",
+    content: "test test test",
+    created: "2022-11-23 12:17",
+    modified: "2022-11-23 12:38",
+    tags: ["test", "테스트"],
+  },
+  {
+    id: 4,
+    writer: 1, // user_uuid
+    category: "test",
+    title: "첫번째 게시물입니다.",
+    content: "test test test",
+    created: "2022-11-23 12:17",
+    modified: "2022-11-23 12:38",
+    tags: ["test", "테스트"],
+  },
+  {
+    id: 5,
+    writer: 1, // user_uuid
+    category: "test",
+    title: "첫번째 게시물입니다.",
+    content: "test test test",
+    created: "2022-11-23 12:17",
+    modified: "2022-11-23 12:38",
+    tags: ["test", "테스트"],
+  },
+  {
+    id: 6,
+    writer: 1, // user_uuid
+    category: "test",
+    title: "첫번째 게시물입니다.",
+    content: "test test test",
+    created: "2022-11-23 12:17",
+    modified: "2022-11-23 12:38",
+    tags: ["test", "테스트"],
+  },
+  {
+    id: 7,
+    writer: 1, // user_uuid
+    category: "test",
+    title: "첫번째 게시물입니다.",
+    content: "test test test",
+    created: "2022-11-23 12:17",
+    modified: "2022-11-23 12:38",
+    tags: ["test", "테스트"],
+  },
+];
