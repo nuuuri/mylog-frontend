0a34e71d311974421b774dc741b970a5cd0ba50e
nuuuri <sbfl125@gmail.com>
2022-11-23 17:45:19 +0900

    DummyData 갯수 증가

--- src/assets/dummy.ts
+++ src/assets/dummy.ts
@@ -88,4 +88,112 @@ export const posts = [
     modified: "2022-11-23 12:38",
     tags: ["test", "테스트"],
   },
+  {
+    id: 3,
+    writer: 1, // user_uuid
+    category: "test",
+    title:
+      "뭔가 긴 제목 뭔가 긴 제목 뭔가 긴 제목 뭔가 긴 제목 뭔가 긴 제목 뭔가 긴 제목입니당!!!",
+    content:
+      "test입니다 신나용! test입니다 신나용! test입니다 신나용! test입니다 신나용! test입니다 신나용! test입니다 신나용! test입니다 신나용! test입니다 신나용! test입니다 신나용!test입니다 신나용!test입니다 신나용! ",
+    created: "2022-11-23 12:17",
+    modified: "2022-11-23 12:38",
+    tags: ["test", "테스트"],
+  },
+  {
+    id: 4,
+    writer: 1, // user_uuid
+    category: "test",
+    title: "첫번째 게시물입니다.",
+    content: "test test test",
+    created: "2022-11-23 12:17",
+    modified: "2022-11-23 12:38",
+    tags: ["test", "테스트"],
+    thumbnail: "123",
+  },
+  {
+    id: 5,
+    writer: 1, // user_uuid
+    category: "test",
+    title: "첫번째 게시물입니다.",
+    content: "test test test",
+    created: "2022-11-23 12:17",
+    modified: "2022-11-23 12:38",
+    tags: ["test", "테스트"],
+  },
+  {
+    id: 6,
+    writer: 1, // user_uuid
+    category: "test",
+    title: "첫번째 게시물입니다.",
+    content: "test test test",
+    created: "2022-11-23 12:17",
+    modified: "2022-11-23 12:38",
+    tags: ["test", "테스트"],
+  },
+  {
+    id: 7,
+    writer: 1, // user_uuid
+    category: "test",
+    title: "첫번째 게시물입니다.",
+    content:
+      "test입니다 신나용! test입니다 신나용! test입니다 신나용! test입니다 신나용! test입니다 신나용! test입니다 신나용! test입니다 신나용! test입니다 신나용! test입니다 신나용!test입니다 신나용!test입니다 신나용! ",
+    created: "2022-11-23 12:17",
+    modified: "2022-11-23 12:38",
+    tags: ["test", "테스트"],
+  },
+  {
+    id: 3,
+    writer: 1, // user_uuid
+    category: "test",
+    title:
+      "뭔가 긴 제목 뭔가 긴 제목 뭔가 긴 제목 뭔가 긴 제목 뭔가 긴 제목 뭔가 긴 제목입니당!!!",
+    content:
+      "test입니다 신나용! test입니다 신나용! test입니다 신나용! test입니다 신나용! test입니다 신나용! test입니다 신나용! test입니다 신나용! test입니다 신나용! test입니다 신나용!test입니다 신나용!test입니다 신나용! ",
+    created: "2022-11-23 12:17",
+    modified: "2022-11-23 12:38",
+    tags: ["test", "테스트"],
+  },
+  {
+    id: 4,
+    writer: 1, // user_uuid
+    category: "test",
+    title: "첫번째 게시물입니다.",
+    content: "test test test",
+    created: "2022-11-23 12:17",
+    modified: "2022-11-23 12:38",
+    tags: ["test", "테스트"],
+    thumbnail: "123",
+  },
+  {
+    id: 5,
+    writer: 1, // user_uuid
+    category: "test",
+    title: "첫번째 게시물입니다.",
+    content: "test test test",
+    created: "2022-11-23 12:17",
+    modified: "2022-11-23 12:38",
+    tags: ["test", "테스트"],
+  },
+  {
+    id: 6,
+    writer: 1, // user_uuid
+    category: "test",
+    title: "첫번째 게시물입니다.",
+    content: "test test test",
+    created: "2022-11-23 12:17",
+    modified: "2022-11-23 12:38",
+    tags: ["test", "테스트"],
+  },
+  {
+    id: 7,
+    writer: 1, // user_uuid
+    category: "test",
+    title: "마지막 게시물입니다.",
+    content:
+      "test입니다 신나용! test입니다 신나용! test입니다 신나용! test입니다 신나용! test입니다 신나용! test입니다 신나용! test입니다 신나용! test입니다 신나용! test입니다 신나용!test입니다 신나용!test입니다 신나용! ",
+    created: "2022-11-23 12:17",
+    modified: "2022-11-23 12:38",
+    tags: ["test", "테스트"],
+  },
 ];
