cfbc54ded84b2abf4277729278999e4fc049c804
nuuuri <sbfl125@gmail.com>
2022-11-24 17:55:13 +0900

    [feat] PostList 추가

---
+++ src/components/PostList/index.tsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import styled from "styled-components";
+import PostCard from "./PostCard";
+import Pagination from "components/Pagination";
+
+export default function PostList(props: {
+  title: String;
+  posts: any[];
+  limit?: number;
+}) {
+  const { title, posts } = props;
+  const [currentPage, setCurrentPage] = useState(1);
+  const limit = props.limit ?? 5;
+  const offset = limit * (currentPage - 1);
+
+  return (
+    <Container>
+      <Header>
+        <p className="total-posts-title">{title}</p>
+        <div className="total-posts-number">{posts.length}</div>
+      </Header>
+      <Line />
+      <Body>
+        {posts.slice(offset, offset + limit).map((post) => (
+          <PostCard key={post.id} post={post} />
+        ))}
+      </Body>
+      <Pagination
+        totalPage={Math.ceil(posts.length / limit)}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  font-family: "MonoplexKR";
+`;
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: default;
+
+  .total-posts-title {
+    font-size: 17px;
+    font-weight: 400;
+  }
+  .total-posts-number {
+    width: fit-content;
+    height: fit-content;
+    margin-left: 10px;
+    padding: 1px 8px;
+    border-radius: 6px;
+    background: #add8e6;
+    color: #ffffff;
+    font-weight: 600;
+    font-size: 13px;
+  }
+`;
+const Line = styled.div`
+  width: 100%;
+  height: 1px;
+  background: #e2e2e2;
+`;
+const Body = styled.div`
+  margin: 25px 0 50px 0;
+`;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,65 +1,8 @@
-import styled from "styled-components";
 import { posts } from "assets/dummy";
-import PostCard from "components/PostCard";
-import Pagination from "components/Pagination";
-import { useState } from "react";
+import PostList from "components/PostList";
 
 export default function HomePage() {
-  const [currentPage, setCurrentPage] = useState(1);
-  const limit = 5;
-  const offset = limit * (currentPage - 1);
   const totalPosts = posts;
 
-  return (
-    <Container>
-      <Header>
-        <p className="total-posts-title">전체 글</p>
-        <div className="total-posts-number">{totalPosts.length}</div>
-      </Header>
-      <Line />
-      <Body>
-        {totalPosts.slice(offset, offset + limit).map((post) => (
-          <PostCard key={post.id} post={post} />
-        ))}
-      </Body>
-      <Pagination
-        totalPage={Math.ceil(totalPosts.length / limit)}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-      />
-    </Container>
-  );
+  return <PostList title="전체 글" posts={totalPosts} />;
 }
-
-const Container = styled.div`
-  font-family: "MonoplexKR";
-`;
-const Header = styled.div`
-  display: flex;
-  align-items: center;
-  cursor: default;
-
-  .total-posts-title {
-    font-size: 17px;
-    font-weight: 400;
-  }
-  .total-posts-number {
-    width: fit-content;
-    height: fit-content;
-    margin-left: 10px;
-    padding: 1px 8px;
-    border-radius: 6px;
-    background: #add8e6;
-    color: #ffffff;
-    font-weight: 600;
-    font-size: 13px;
-  }
-`;
-const Line = styled.div`
-  width: 100%;
-  height: 1px;
-  background: #e2e2e2;
-`;
-const Body = styled.div`
-  margin: 25px 0 50px 0;
-`;
