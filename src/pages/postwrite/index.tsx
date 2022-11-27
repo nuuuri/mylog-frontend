e5142ddbf3229a364f17fd0b7959902eeacf9cf6
nuuuri <sbfl125@gmail.com>
2022-11-27 17:01:36 +0900

    [feat] PostWritePage 레이아웃 추가

--- src/pages/postwrite/index.tsx
+++ src/pages/postwrite/index.tsx
@@ -1,3 +1,85 @@
+import styled from "styled-components";
+
 export default function PostWritePage() {
-  return <div>글쓰기 페이지</div>;
+  return (
+    <Container>
+      <PostMenu>
+        <div>사진</div>
+        <div>동영상</div>
+        <div>스티커</div>
+        <div>인용구</div>
+        <div>구분선</div>
+        <div>장소</div>
+        <div>링크</div>
+        <div>파일</div>
+        <div>일정</div>
+        <div>소스코드</div>
+        <div>표</div>
+        <div>수식</div>
+      </PostMenu>
+      <Toolbar>
+        <div>본문(문단 서식)</div>
+        <div>나눔고딕(서체)</div>
+        <div>15(글자 크기)</div>
+
+        <div>B(굵기 적용)</div>
+        <div>I(기울이기 적용)</div>
+        <div>U(밑줄 적용)</div>
+        <div>T(취소선 적용)</div>
+        <div>글자색 변경</div>
+        <div>글자 배경색 변경</div>
+
+        <div>정렬</div>
+        <div>줄간격</div>
+        <div>목록</div>
+
+        <div>머리글자 적용</div>
+        <div>위첨자 적용</div>
+        <div>아래첨자 적용</div>
+        <div>특수문자</div>
+        <div>링크 입력</div>
+        <div>맞춤법</div>
+      </Toolbar>
+
+      <Canvas>
+        <div contentEditable={true} placeholder="제목 없음" />
+      </Canvas>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background: #e9e9e9; ;
+`;
+const PostMenu = styled.div`
+  display: flex;
+  width: 100%;
+  height: 80px;
+  background: yellow;
+`;
+const Toolbar = styled.div`
+  display: flex;
+  width: 100%;
+  height: 40px;
+  background: pink;
+`;
+const Canvas = styled.div`
+  width: 80%;
+  height: calc(100% - 140px);
+  margin-top: 10px;
+  background: #fff;
+
+  div {
+    background: lightblue;
+    outline: none;
+
+    ::after {
+      content: attr(placeholder);
+    }
+  }
+`;
