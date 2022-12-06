35dc8ac2ba4ae662a426e049f3f441c8b25e2b8d
nuuuri <sbfl125@gmail.com>
2022-12-06 20:35:58 +0900

    [feat] postService API 추가(연동x)

---
+++ src/common/axios/postService/index.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+class PostService {
+  BASE_URL = "/post";
+
+  createPost(data: {
+    userId: string;
+    categoryId: number;
+    title: string;
+    blocks: { html: string; tag: string }[];
+  }) {
+    return axios.post(`${this.BASE_URL}`, { data });
+  }
+
+  getPost(id: number) {
+    return axios.get(`${this.BASE_URL}/${id}`);
+  }
+}
+
+export default new PostService();
