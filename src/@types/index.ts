8b4991f19933973ea815a867f7e4477970c6172f
nuuuri <sbfl125@gmail.com>
2022-12-13 14:28:35 +0900

    Category interface 추가

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -7,3 +7,11 @@ export interface Block {
   html: string;
   tag: string;
 }
+
+export interface Category {
+  id: number;
+  name: string;
+  label: string;
+  count: number;
+  subCategories: Category[];
+}
